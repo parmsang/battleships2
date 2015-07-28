0f2500f8fad73b20facfd513a265a1cc818d002d
Winifred Au <winifredau1990@gmail.com>
2015-07-28 11:20:43 -0400

    added direction to initialize method in ship class and changes to ship spec

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,20 +1,21 @@
 
 class Ship
 
-  attr_accessor :size
+  attr_accessor :size, :direction
 
-  def initialize(size)
+  def initialize(size, direction)
     @size = size
+    @direction = direction
     # @directions = ['North', 'South', 'East', 'West']
   end
 
 
- 
+
     # puts "Choose from these ship sizes (in units): 2, 3, 4, 5"
     #   ship_size = gets.chomp
     # fail "That is not a valid selection" if @sizes.delete(ship_size) != ship_size
-    # ship_size 
- 
+    # ship_size
+
 
   # def direction ()
   #   puts "Choose direction to face your ship: North, South, East, West"
@@ -22,6 +23,3 @@ class Ship
   # end
 
 end
-
-
-
--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -1,11 +1,12 @@
 require 'ship'
 
 describe Ship do
-  describe '#initialize#size' do
-  	let(:ship) {Ship.new(3)}
-    it 'allows players to choose from a range of ship sizes' do	
+  describe '#initialize#size#direction' do
+  	let(:ship) {Ship.new(3, "vertical")}
+    it 'allows players to choose from a range of ship sizes and ship direction' do
     	expect(ship.size).to eq 3
+      expect(ship.direction).to eq "vertical"
     end
   end
+
 end
-	
